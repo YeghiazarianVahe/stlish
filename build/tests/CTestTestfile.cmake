33825c65849d8d22730159acf403b64c961fc18b
YeghiazarianVahe <yeghiazarian.vahe@gmail.com>
2026-02-01 19:29:58 +0400

    chore: remove build folder from tracking and update .gitignore

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Vahe Yeghiazaryan/Documents/dev/Portfolio/stlish/tests
-# Build directory: C:/Users/Vahe Yeghiazaryan/Documents/dev/Portfolio/stlish/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Vahe Yeghiazaryan/Documents/dev/Portfolio/stlish/build/tests/stlish_smoke_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
